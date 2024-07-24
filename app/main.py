b9a1e71c7d313968452cad2a02a4c7085a734ed5
Yahir Ordoñez <41529436+Yahirpuc@users.noreply.github.com>
2024-07-23 22:38:46 -0600

    Update main.py

--- app/main.py
+++ app/main.py
@@ -41,13 +41,6 @@ origins = [
     "http://0.0.0.0:8000",  # Reemplaza con la URL exacta de tu frontend
  
 ]
-# Montar directorios estáticos
-app.mount("/images", StaticFiles(directory="images"), name="images")
-app.mount("/imgs", StaticFiles(directory="imgs"), name="imgs")
-
-app.mount("/PaginasDelInicio", StaticFiles(directory="PaginasDelInicio"), name="PaginasDelInicio")
-app.mount("/PaginasNav", StaticFiles(directory="PaginasNav"), name="PaginasNav")
-app.mount("/PanelAdministracion", StaticFiles(directory="PanelAdministracion"), name="PanelAdministracion")
 
 @app.get("/")
 def read_root():
