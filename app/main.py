a9ffeba75391a41e65159796f6c8eb0240d9d7cc
Yahir Ordoñez <41529436+Yahirpuc@users.noreply.github.com>
2024-07-23 20:15:36 -0600

    Update main.py

--- app/main.py
+++ app/main.py
@@ -709,6 +709,6 @@ def get_datos_graficas():
         raise HTTPException(status_code=500, detail=str(e))
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
 
 
